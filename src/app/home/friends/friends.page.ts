b638a8ccaac1ef390864c028411e39ba1c8837ad
Agung N <muhammadagungnursyamsi@gmail.com>
2020-12-18 12:35:06 +0700

    test vercel

--- src/app/home/friends/friends.page.ts
+++ src/app/home/friends/friends.page.ts
@@ -45,54 +45,54 @@ export class FriendsPage implements OnInit {
 
   ngOnInit() {
 
-    this.authService.userDetails().subscribe(res => {
-      console.log(res);
-      if(res !== null){
-        this.userId = res.uid;
-
-        this.friendSrv.getAllFriend(this.userId).snapshotChanges().pipe(
-            map(changes =>
-                changes.map(c => ({key: c.payload.key, ...c.payload.val()}))
-            )
-        ).subscribe(data => {
-          this.friend = data;
-          this.userFriend = this.friend;
-          console.log(this.userFriend);
-
-          this.userSrv.getAllUser().snapshotChanges().pipe(
-              map(changes =>
-                  changes.map(c => ({key: c.payload.key, ...c.payload.val()}))
-              )
-          ).subscribe(data => {
-            this.userData = data;
-            this.userList = this.userData;
-            console.log(this.userList, this.userFriend[0].email);
-            let j = 0;
-            for(let i = 0; i < this.userList.length;){
-              if(this.userList[i].email == this.userFriend[j].email){
-                console.log('sama');
-                this.friendList[j] = this.userData[j];
-                this.resetFriend[j] = this.userData[j];
-                console.log(this.friendList[j]);
-                i=0;
-                j++;
-                if(j == this.userFriend.length){
-                  break;
-                }
-              }else{
-                i++;
-              }
-            }
-
-            this.setFilteredItems("");
-            this.searchControl.valueChanges.pipe(debounceTime(200)).subscribe(search => {
-              this.setFilteredItems(search);
-            });
-            //this.compareData(this.friend.length, this.userData.length);
-          });
-        });
-      }
-    })
+    // this.authService.userDetails().subscribe(res => {
+    //   console.log(res);
+    //   if(res !== null){
+    //     this.userId = res.uid;
+    //
+    //     this.friendSrv.getAllFriend(this.userId).snapshotChanges().pipe(
+    //         map(changes =>
+    //             changes.map(c => ({key: c.payload.key, ...c.payload.val()}))
+    //         )
+    //     ).subscribe(data => {
+    //       this.friend = data;
+    //       this.userFriend = this.friend;
+    //       console.log(this.userFriend);
+    //
+    //       this.userSrv.getAllUser().snapshotChanges().pipe(
+    //           map(changes =>
+    //               changes.map(c => ({key: c.payload.key, ...c.payload.val()}))
+    //           )
+    //       ).subscribe(data => {
+    //         this.userData = data;
+    //         this.userList = this.userData;
+    //         console.log(this.userList, this.userFriend[0].email);
+    //         let j = 0;
+    //         for(let i = 0; i < this.userList.length;){
+    //           if(this.userList[i].email == this.userFriend[j].email){
+    //             console.log('sama');
+    //             this.friendList[j] = this.userData[j];
+    //             this.resetFriend[j] = this.userData[j];
+    //             console.log(this.friendList[j]);
+    //             i=0;
+    //             j++;
+    //             if(j == this.userFriend.length){
+    //               break;
+    //             }
+    //           }else{
+    //             i++;
+    //           }
+    //         }
+    //
+    //         this.setFilteredItems("");
+    //         this.searchControl.valueChanges.pipe(debounceTime(200)).subscribe(search => {
+    //           this.setFilteredItems(search);
+    //         });
+    //         //this.compareData(this.friend.length, this.userData.length);
+    //       });
+    //     });
+    //   }
+    // })
 
     this.authService.getUserData().snapshotChanges().pipe(
         map(changes =>
